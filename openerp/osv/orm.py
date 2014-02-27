22670ba3607ce1c53398e61d5e2e62025c737a7a
sys-zerp-staging <it-admin@zaber.com>
2014-02-27 11:17:57 -0800

    fix bad merge

--- openerp/osv/orm.py
+++ openerp/osv/orm.py
@@ -71,11 +71,7 @@ _schema = logging.getLogger(__name__ + '.schema')
 crud_logger = logging.getLogger('crud')
 crud_logger.setLevel(logging.INFO)
 crud_logger.propagate = False
-<<<<<<< HEAD
-crud_file = logging.FileHandler('/var/log/openerp/crudlogger.csv')
-=======
 crud_file = logging.FileHandler('crudlogger.csv')
->>>>>>> 396--Log-all-models-crud
 crud_logger.addHandler(crud_file)
 
 
