c9092114d8ab7b1218ea7d9e5e7624392550a969
Colin Ligertwood <colin@zaber.com>
2015-09-14 13:46:39 -0700

    [imp] start integrating ddp protocol with zerp

---
+++ openerp/addons/zerp_ddp/__init__.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+#################################################################################
+#
+#    Copyright (C) 2015 - Aki Mimoto 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+import ddp
+import threading
+
+server = None
+server_thread = None
+
+def launch_ddp():
+    global server
+    global server_thread
+
+    # Create then start the server
+    server = ddp.Server()
+    server_thread = threading.Thread(target=lambda *a: server.run())  
+    server_thread.daemon = False
+    server_thread.start()
+
+
+
