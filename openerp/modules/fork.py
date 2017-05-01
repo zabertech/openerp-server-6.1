77e97803765e59ed2169b913ef29722135f61db2
Colin Ligertwood <colin@zaber.com>
2017-05-01 09:54:12 -0700

    fork: better management of child processes
    refs #3256 see https://bugs.izaber.com/issues/3256

--- openerp/modules/fork.py
+++ openerp/modules/fork.py
@@ -20,7 +20,10 @@
 
 from multiprocessing import Pipe, Process
 import os
+import sys
 import signal
+from time import sleep
+import openerp.tools.config
 
 class fork(object):
 
@@ -36,13 +39,22 @@ class fork(object):
 
             # If a list of argv index values is passed, append those arguments to the
             # process name
-            __process_name = self.name
+            process_name = self.name
             for arg in self.name_args:
-                __process_name += " {}".format(args[arg])
+                process_name += " {}".format(args[arg])
 
             # Wrap the decorated function in another function which will replace
             # the passed cursor argument with a new cursor unique to the new process
             def forked(dbname, send, *args, **kwargs):
+
+                # Make sure subprocesses exit on sigint
+                def int_handler(signal, fname):
+                    # Sleep to allow the parent process to join which hopefully
+                    # prevents zombies
+                    sleep(1)
+                    sys.exit()
+
+                signal.signal(signal.SIGINT, int_handler)
                 # Shut down twisted in this process if it's running
                 from twisted.internet import reactor
                 from twisted.internet.error import ReactorNotRunning
@@ -52,9 +64,9 @@ class fork(object):
                     pass
                 
                 # Set the proc title of this process for debugging
-                if __process_name:
+                if process_name:
                     from setproctitle import setproctitle
-                    setproctitle(__process_name)
+                    setproctitle(process_name)
 
                 # Get our own db cursor just for this process
                 from sql_db import ConnectionPool, Connection, Cursor
@@ -80,6 +92,9 @@ class fork(object):
                 send.send(ret)
                 send.close()
 
+                # can't hurt
+                sys.exit()
+
             # Get the current db name from the passed cursor
             dbname = args[1].dbname
 
@@ -88,18 +103,26 @@ class fork(object):
             (recv, send) = Pipe(False)
 
             # Create a new forked process targetting the forked function wrapper
-            p = Process(target=forked, args=(dbname, send, args), name=__process_name)
+            p = Process(target=forked, args=(dbname, send, args), name=process_name)
+
+            # Daemon processes should die when the parent is killed
+            p.daemon = True
 
             # Start the new process and close our copy of the child's pipe end
             p.start()
+            child_process_pid = p.pid
             send.close()
 
             try:
                 # Listen on the pipe for the return value from the new process
-                ret = recv.recv()
+                if recv.poll(int(openerp.tools.config.get('forked_job_timeout', 3600))):
+                    ret = recv.recv()
+                else:
+                    raise Exception('Timeout', 'Forked process timed out before returning: {}'.format(process_name))
             except Exception, err:
                 # Default to an exception just incase the forked process doesn't respond
                 ret = err
+                os.kill(child_process_pid, signal.SIGINT)
             finally:
                 recv.close()
                 p.join()
